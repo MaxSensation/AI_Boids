24ff284be5d04f7b0d2a8c15c2829fa309a63631
Maximiliam Rosén <karlsson.maximiliam@gmail.com>
2021-06-21 17:43:54 +0200

    Fixed some scripts

--- Assets/Scripts/Segment.cs
+++ Assets/Scripts/Segment.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Segment : MonoBehaviour
 {
     private readonly List<Fish> _fish = new List<Fish>();
-    private List<Segment> _neighbors = new List<Segment>();
 
     public void AddFish(Fish fish)
     {
@@ -24,16 +23,13 @@ public class Segment : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        if (_fish.Count > 0)
-            Gizmos.color = new Color(0, 1, 0, 0.2f);
-        else
-            Gizmos.color = new Color(1, 0, 0, 0.01f);
+        Gizmos.color = _fish.Count > 0 ? new Color(0, 1, 0, 0.2f) : new Color(1, 0, 0, 0.01f);
         Gizmos.DrawCube(transform.position, transform.localScale);
     }
 
     public Fish GetClosestFish(Fish searchingFish)
     {
         var newColl = _fish.Where(fish => fish != searchingFish);
-        return newColl.Count() == 0 ? null : newColl.OrderBy(fish => (searchingFish.transform.position - fish.transform.position).sqrMagnitude).First();
+        return !newColl.Any() ? null : newColl.OrderBy(fish => (searchingFish.transform.position - fish.transform.position).sqrMagnitude).First();
     }
 }
